f7a344a3b28a135452ae30c00a3293b8ffd84dd4
koolkrafter5 <creepersrgreen@gmail.com>
2024-11-21 02:44:28 -0500

    Create a new class to allow for configurable meteor reagents.

---
+++ src/main/java/WayofTime/alchemicalWizardry/common/summoning/meteor/MeteorReagent.java
@@ -0,0 +1,8 @@
+package WayofTime.alchemicalWizardry.common.summoning.meteor;
+
+public class MeteorReagent {
+    public int radiusChange;
+    public int fillerChanceChange;
+    public float fillerChanceMultiplier;
+    public
+}
